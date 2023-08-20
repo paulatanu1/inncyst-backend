787b93796a2f0950849a0ec28dc03ed19290c7a0
MANAS PRAMANIK <manas@pitangent.com>
2023-08-20 18:12:42 +0530

    fix

--- modules/student/student.model.js
+++ modules/student/student.model.js
@@ -4,12 +4,12 @@ const studentSchema = Schema({
     intranshipId: {type: Schema.Types.ObjectId, ref: 'intranship'},
     userId: {type: Schema.Types.ObjectId, ref: 'Auth'},
     studentName: { type: String, required: true },
-    // intranshipId: {type: String},
-    // userId: {type: String},
     email: { type: String, required: true },
     phone: { type: String, required: true },
     resume: { type: String, required: true },
     availability: { type: Number, default: 1 },
+    status: { type: String, required: true },
+    applicationStatus: { type: String, enum: [] },
     availability_message: { type: String, default: null }
 });
 
