1a4cd1d0b2a3c75d638f977d04c3067a56068b9e
MANAS PRAMANIK <manas@pitangent.com>
2023-11-17 22:17:07 +0530

    fix industry posts

--- modules/student/student.model.js
+++ modules/student/student.model.js
@@ -1,16 +1,27 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const studentSchema = Schema({
-    jobId: { type: Schema.Types.ObjectId, required: true },
-    userId: { type: Schema.Types.ObjectId, ref: 'Auth' },
-    studentName: { type: String },
-    email: { type: String },
-    phone: { type: String },
-    resume: { type: String, required: true },
-    availability: { type: Number, default: 1 },
-    status: { type: Boolean, default: false },
-    applicationStatus: { type: String, enum: ['pending', 'review', 'interviewScheduled', 'complete', 'notSelected', 'rejected'], default: 'pending' },
-    availability_message: { type: String, default: null }
+  jobId: { type: Schema.Types.ObjectId, required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "Auth" },
+  studentName: { type: String },
+  email: { type: String },
+  phone: { type: String },
+  resume: { type: String, required: true },
+  availability: { type: Number, default: 1 },
+  status: { type: Boolean, default: false },
+  applicationStatus: {
+    type: String,
+    enum: [
+      "pending",
+      "interviewScheduled",
+      "offerReceived",
+      "hired",
+      "notSelected",
+      "rejected",
+    ],
+    default: "pending",
+  },
+  availability_message: { type: String, default: null },
 });
 
 module.exports = model("student", studentSchema);
