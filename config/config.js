f049cf8a8700d8bdaff61a1df16bd27aca77ac59
MANAS PRAMANIK <manas@pitangent.com>
2024-01-02 00:08:39 +0530

    fix

--- config/config.js
+++ config/config.js
@@ -3,5 +3,6 @@ module.exports = {
     MAIL_HOST: 'smtp',
     MAIL_USERNAME: process.env.EMAIL,
     MAIL_PASSWORD: process.env.PASSWORD,
-    MAIL_FROM_ADDRESS: "immanasp@gmail.com"
+    MAIL_FROM_ADDRESS: "immanasp@gmail.com",
+    F2SMS_API_KEY: "voAWPEZfYeSOl9kTLu470FJrKqUV68HMsyR23xDgXbtCNwcGIBJWqj0axZ6Es3RPCSAfDncX1QFNvy54"
 }
