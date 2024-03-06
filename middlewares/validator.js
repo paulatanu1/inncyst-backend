60e2cb6c36b30252fac6f207f09ed1e8d90854e7
MANAS PRAMANIK <manas@pitangent.com>
2024-03-06 22:39:02 +0530

    fixing

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -18,178 +18,160 @@ const VALIDATIONS = {
 }
 
 class Validator {
-    static registratonRequest(params) {
-        const schema = Joi.object().keys({
-            name: VALIDATIONS.NAME,
-            role: VALIDATIONS.STRING_REQUIRED,
-            email: VALIDATIONS.EMAIL,
-            phone: VALIDATIONS.PHONE_NUMBER,
-            password: VALIDATIONS.PASSWORD
-        });
-        return schema.validate(params);
-    }
+  static registratonRequest(params) {
+    const schema = Joi.object().keys({
+      name: VALIDATIONS.NAME,
+      role: VALIDATIONS.STRING_REQUIRED,
+      email: VALIDATIONS.EMAIL,
+      phone: VALIDATIONS.PHONE_NUMBER,
+      password: VALIDATIONS.PASSWORD,
+    });
+    return schema.validate(params);
+  }
 
-    static loginRequests(params) {
-        const schema = Joi.object().keys({
-            email: VALIDATIONS.EMAIL,
-            password: VALIDATIONS.PASSWORD
-        });
-        return schema.validate(params);
-    }
+  static loginRequests(params) {
+    const schema = Joi.object().keys({
+      email: VALIDATIONS.EMAIL,
+      password: VALIDATIONS.PASSWORD,
+    });
+    return schema.validate(params);
+  }
 
-    static industryQuestions(params) {
-        const schema = Joi.object().keys({
-            companyName: VALIDATIONS.STRING_REQUIRED,
-            companyEstdYear: VALIDATIONS.STRING_REQUIRED,
-            aboutCompany: VALIDATIONS.STRING_REQUIRED,
-            empCount: VALIDATIONS.NUMBER_REQUIRED,
-            workPlace: VALIDATIONS.STRING_REQUIRED,
-            image: VALIDATIONS.STRING_REQUIRED,
-            branchOffice: VALIDATIONS.STRING,
-            corporateOffice: VALIDATIONS.STRING
-        });
-        return schema.validate(params);
-    }
+  static industryQuestions(params) {
+    const schema = Joi.object().keys({
+      companyName: VALIDATIONS.STRING_REQUIRED,
+      companyEstdYear: VALIDATIONS.STRING_REQUIRED,
+      aboutCompany: VALIDATIONS.STRING_REQUIRED,
+      empCount: VALIDATIONS.NUMBER_REQUIRED,
+      workPlace: VALIDATIONS.STRING_REQUIRED,
+      image: VALIDATIONS.STRING_REQUIRED,
+      branchOffice: VALIDATIONS.STRING,
+      corporateOffice: VALIDATIONS.STRING,
+    });
+    return schema.validate(params);
+  }
 
-    static manufacturerQuestion(params) {
-        const schema = Joi.object().keys({
-            manufacturerId: VALIDATIONS.STRING_REQUIRED,
-            companyName: VALIDATIONS.STRING_REQUIRED,
-            companyEstdYear: VALIDATIONS.STRING_REQUIRED,
-            aboutCompany: VALIDATIONS.STRING_REQUIRED,
-            empCount: VALIDATIONS.NUMBER_REQUIRED,
-            workPlace: VALIDATIONS.STRING_REQUIRED,
-            facilitiesWithPrice: VALIDATIONS.STRING_REQUIRED
-        });
-        return schema.validate(params);
-    }
+  static manufacturerQuestion(params) {
+    const schema = Joi.object().keys({
+      manufacturerId: VALIDATIONS.STRING_REQUIRED,
+      companyName: VALIDATIONS.STRING_REQUIRED,
+      companyEstdYear: VALIDATIONS.STRING_REQUIRED,
+      aboutCompany: VALIDATIONS.STRING_REQUIRED,
+      empCount: VALIDATIONS.NUMBER_REQUIRED,
+      workPlace: VALIDATIONS.STRING_REQUIRED,
+      facilitiesWithPrice: VALIDATIONS.STRING_REQUIRED,
+    });
+    return schema.validate(params);
+  }
 
-    static intranshipRequest(params) {
-        const schema = Joi.object().keys({
-            companyName: VALIDATIONS.STRING_REQUIRED,
-            intranshipOverView: VALIDATIONS.DESCRIPTION,
-            intranshipName: VALIDATIONS.STRING_REQUIRED,
-            intranshipType: VALIDATIONS.STRING_REQUIRED,
-            location: VALIDATIONS.STRING_REQUIRED,
-            skills: VALIDATIONS.ARRAY,
-            jobType: VALIDATIONS.STRING_REQUIRED,
-            salary: VALIDATIONS.STRING_REQUIRED,
-            information: VALIDATIONS.STRING_REQUIRED,
-            opens: VALIDATIONS.NUMBER_REQUIRED,
-            perks: VALIDATIONS.ARRAY_REQUIRED,
-        });
-        return schema.validate(params);
-    }
+  static intranshipRequest(params) {
+    const schema = Joi.object().keys({
+      companyName: VALIDATIONS.STRING_REQUIRED,
+      intranshipOverView: VALIDATIONS.DESCRIPTION,
+      intranshipName: VALIDATIONS.STRING_REQUIRED,
+      intranshipType: VALIDATIONS.STRING_REQUIRED,
+      location: VALIDATIONS.STRING_REQUIRED,
+      skills: VALIDATIONS.ARRAY,
+      jobType: VALIDATIONS.STRING_REQUIRED,
+      salary: VALIDATIONS.STRING_REQUIRED,
+      information: VALIDATIONS.STRING_REQUIRED,
+      opens: VALIDATIONS.NUMBER_REQUIRED,
+      perks: VALIDATIONS.ARRAY_REQUIRED,
+    });
+    return schema.validate(params);
+  }
 
-    static applyForIntranship(params) {
-        const schema = Joi.object().keys({
-            email: VALIDATIONS.EMAIL,
-            phone: VALIDATIONS.PHONE_NUMBER
-        });
-        return schema.validate(params);
-    }
+  static applyForIntranship(params) {
+    const schema = Joi.object().keys({
+      email: VALIDATIONS.EMAIL,
+      phone: VALIDATIONS.PHONE_NUMBER,
+    });
+    return schema.validate(params);
+  }
 
-    static industryPostIntranship(params) {
-        const schema = Joi.object().keys({
-            id: VALIDATIONS.STRING,
-            title: VALIDATIONS.STRING_REQUIRED,
-            type: VALIDATIONS.STRING_REQUIRED,
-            skills: VALIDATIONS.ARRAY_REQUIRED,
-            details: VALIDATIONS.STRING_REQUIRED,
-            intranshipType: VALIDATIONS.STRING_REQUIRED,
-            startDate: VALIDATIONS.STRING_REQUIRED,
-            duration: VALIDATIONS.STRING_REQUIRED,
-            durationIn: VALIDATIONS.STRING_REQUIRED,
-                    // education: VALIDATIONS.STRING_REQUIRED,
-            jobOpening: VALIDATIONS.NUMBER_REQUIRED,
-            stipend: VALIDATIONS.STRING_REQUIRED,
-            salary: VALIDATIONS.NUMBER_REQUIRED,
-            salaryIn: VALIDATIONS.NUMBER_REQUIRED,
-            salaryType: VALIDATIONS.STRING_REQUIRED,
-            perks: VALIDATIONS.ARRAY_REQUIRED,
-            location: VALIDATIONS.STRING_REQUIRED,
-            status: VALIDATIONS.BOOLEAN_REQUIRED,
-            letter: VALIDATIONS.ANY,
-            availability: VALIDATIONS.ANY,
-            moreQuestions: VALIDATIONS.ANY,
-            addtionalCandidatePreference: VALIDATIONS.STRING_REQUIRED,
-            alternativePhone: VALIDATIONS.STRING_REQUIRED,
-                    // description: VALIDATIONS.STRING_REQUIRED,
-            jobType: VALIDATIONS.STRING_REQUIRED,
-            responsiblity: VALIDATIONS.ANY,
-            womanRestart: VALIDATIONS.STRING_REQUIRED,
-                    // salaryCtcDescription: Joi.object().keys({
-                    //     currency: VALIDATIONS.STRING_REQUIRED,
-                    //     ctcFrom: VALIDATIONS.STRING_REQUIRED,
-                    //     ctcTo: VALIDATIONS.STRING_REQUIRED,
-                    //     ctcType: VALIDATIONS.STRING_REQUIRED,
-                    //     isProbationPeriod: VALIDATIONS.STRING_REQUIRED,
-                    //     perks: VALIDATIONS.ARRAY_REQUIRED
-                    // })
-        });
-        return schema.validate(params);
-    }
+  static industryPostIntranship(params) {
+    const schema = Joi.object().keys({
+      id: VALIDATIONS.STRING,
+      title: VALIDATIONS.STRING_REQUIRED,
+      type: VALIDATIONS.STRING_REQUIRED,
+      skills: VALIDATIONS.ARRAY_REQUIRED,
+      details: VALIDATIONS.STRING_REQUIRED,
+      intranshipType: VALIDATIONS.STRING_REQUIRED,
+      startDate: VALIDATIONS.STRING_REQUIRED,
+      duration: VALIDATIONS.STRING_REQUIRED,
+      durationIn: VALIDATIONS.STRING_REQUIRED,
+      jobOpening: VALIDATIONS.NUMBER_REQUIRED,
+      stipend: VALIDATIONS.STRING_REQUIRED,
+      salary: VALIDATIONS.NUMBER_REQUIRED,
+      salaryIn: VALIDATIONS.NUMBER_REQUIRED,
+      salaryType: VALIDATIONS.STRING_REQUIRED,
+      perks: VALIDATIONS.ARRAY_REQUIRED,
+      location: VALIDATIONS.STRING_REQUIRED,
+      status: VALIDATIONS.BOOLEAN_REQUIRED,
+      letter: VALIDATIONS.ANY,
+      availability: VALIDATIONS.ANY,
+      moreQuestions: VALIDATIONS.ANY,
+      addtionalCandidatePreference: VALIDATIONS.STRING_REQUIRED,
+      alternativePhone: VALIDATIONS.STRING_REQUIRED,
+      jobType: VALIDATIONS.STRING_REQUIRED,
+      responsiblity: VALIDATIONS.ANY,
+      womanRestart: VALIDATIONS.STRING_REQUIRED
+    });
+    return schema.validate(params);
+  }
 
-    static industryPostJob(params) {
-        const schema = Joi.object().keys({
-            id: VALIDATIONS.STRING,
-            title: VALIDATIONS.STRING_REQUIRED,
-            type: VALIDATIONS.STRING_REQUIRED,
-            skills: VALIDATIONS.ARRAY_REQUIRED,
-            details: VALIDATIONS.STRING_REQUIRED,
-            intranshipType: VALIDATIONS.STRING_REQUIRED,
-            // duration: VALIDATIONS.ANY,
-            // durationIn: VALIDATIONS.STRING_REQUIRED,
-            education: VALIDATIONS.STRING_REQUIRED,
-            experience: VALIDATIONS.NUMBER_REQUIRED,
-            experienceTime: VALIDATIONS.STRING_REQUIRED,
-            jobOpening: VALIDATIONS.NUMBER_REQUIRED,
-            responsibilities: VALIDATIONS.ANY,
-            stipend: VALIDATIONS.STRING_REQUIRED,
-            // salary: VALIDATIONS.NUMBER_REQUIRED,
-            // salaryType: VALIDATIONS.STRING_REQUIRED,
-            perks: VALIDATIONS.ARRAY_REQUIRED,
-            location: VALIDATIONS.STRING_REQUIRED,
-            // startDate: VALIDATIONS.ANY,
-            status: VALIDATIONS.BOOLEAN_REQUIRED,
-            letter: VALIDATIONS.ANY,
-            availability: VALIDATIONS.ANY,
-            moreQuestions: VALIDATIONS.ANY,
-            // salaryIn: VALIDATIONS.NUMBER_REQUIRED,
-            addtionalCandidatePreference: VALIDATIONS.STRING_REQUIRED,
-            description: VALIDATIONS.STRING_REQUIRED,
-            jobType: VALIDATIONS.STRING_REQUIRED,
-            salaryCtcDescription: Joi.object().keys({
-                currency: VALIDATIONS.STRING_REQUIRED,
-                ctcFrom: VALIDATIONS.STRING_REQUIRED,
-                ctcTo: VALIDATIONS.STRING_REQUIRED,
-                ctcType: VALIDATIONS.STRING_REQUIRED,
-                isProbationPeriod: VALIDATIONS.STRING_REQUIRED,
-                perks: VALIDATIONS.ARRAY_REQUIRED
-            })
-        });
-        return schema.validate(params);
-    }
+  static industryPostJob(params) {
+    const schema = Joi.object().keys({
+      id: VALIDATIONS.STRING,
+      title: VALIDATIONS.STRING_REQUIRED,
+      type: VALIDATIONS.STRING_REQUIRED,
+      skills: VALIDATIONS.ARRAY_REQUIRED,
+      details: VALIDATIONS.STRING_REQUIRED,
+      intranshipType: VALIDATIONS.STRING_REQUIRED,
+      education: VALIDATIONS.STRING_REQUIRED,
+      experience: VALIDATIONS.NUMBER_REQUIRED,
+      experienceTime: VALIDATIONS.STRING_REQUIRED,
+      jobOpening: VALIDATIONS.NUMBER_REQUIRED,
+      responsibilities: VALIDATIONS.ANY,
+      stipend: VALIDATIONS.STRING_REQUIRED,
+      perks: VALIDATIONS.ARRAY_REQUIRED,
+      location: VALIDATIONS.STRING_REQUIRED,
+      status: VALIDATIONS.BOOLEAN_REQUIRED,
+      letter: VALIDATIONS.ANY,
+      availability: VALIDATIONS.ANY,
+      moreQuestions: VALIDATIONS.ANY,
+      addtionalCandidatePreference: VALIDATIONS.STRING_REQUIRED,
+      description: VALIDATIONS.STRING_REQUIRED,
+      jobType: VALIDATIONS.STRING_REQUIRED,
+      alternativePhone: VALIDATIONS.STRING_REQUIRED,
+      ctcFrom: VALIDATIONS.STRING_REQUIRED,
+      currencyType: VALIDATIONS.STRING_REQUIRED,
+      ctcTo: VALIDATIONS.STRING_REQUIRED,
+      ctcType: VALIDATIONS.STRING_REQUIRED,
+      probationPeriod: VALIDATIONS.ARRAY_REQUIRED,
+    });
+    return schema.validate(params);
+  }
 
-    static contactUs(params) {
-        const schema = Joi.object().keys({
-            name: VALIDATIONS.STRING_REQUIRED,
-            email: VALIDATIONS.EMAIL,
-            phone: VALIDATIONS.PHONE_NUMBER,
-            message: VALIDATIONS.DESCRIPTION
-        });
-        return schema.validate(params);
-    }
+  static contactUs(params) {
+    const schema = Joi.object().keys({
+      name: VALIDATIONS.STRING_REQUIRED,
+      email: VALIDATIONS.EMAIL,
+      phone: VALIDATIONS.PHONE_NUMBER,
+      message: VALIDATIONS.DESCRIPTION,
+    });
+    return schema.validate(params);
+  }
 
-    static addAchivementBody(params) {
-        const schema = Joi.object().keys({
-            title: VALIDATIONS.STRING_REQUIRED,
-            description: VALIDATIONS.STRING_REQUIRED,
-            date: VALIDATIONS.STRING_REQUIRED,
-            held: VALIDATIONS.STRING_REQUIRED
-        });
-        return schema.validate(params);
-    }
+  static addAchivementBody(params) {
+    const schema = Joi.object().keys({
+      title: VALIDATIONS.STRING_REQUIRED,
+      description: VALIDATIONS.STRING_REQUIRED,
+      date: VALIDATIONS.STRING_REQUIRED,
+      held: VALIDATIONS.STRING_REQUIRED,
+    });
+    return schema.validate(params);
+  }
 }
 
 module.exports = Validator;
--- modules/industry/industryPost.model.js
+++ modules/industry/industryPost.model.js
@@ -2,49 +2,50 @@ const { Schema, model } = require("mongoose");
 
 const industrySchema = Schema({
   industryId: { type: Schema.Types.ObjectId, ref: "Auth" },
-  company: {type: Schema.Types.ObjectId, ref: "industry" },
+  company: { type: Schema.Types.ObjectId, ref: "industry" },
   title: { type: String },
   type: { type: String, enum: ["intranship", "job"] },
   details: { type: String, default: null },
   skills: [],
   intranshipType: {
     type: String,
-    enum: ["office", "hybrid", "remote"], default: "office"
+    enum: ["office", "hybrid", "remote"],
+    default: "office",
   },
   jobType: {
     type: String,
-    enum: ["partTime", "fullTime"], default: "fullTime"
+    enum: ["partTime", "fullTime"],
+    default: "fullTime",
   },
   startDate: { type: String },
-  education: { type: String, enum: ['diploma', 'hs', 'master', 'bachelor'] },
+  education: { type: String, enum: ["diploma", "hs", "master", "bachelor"] },
   duration: { type: String },
-  durationIn: { type: String, enum: ["monthly", "yearly"], default: 'monthly' },
+  durationIn: { type: String, enum: ["monthly", "yearly"], default: "monthly" },
   experience: { type: Number },
-  experienceTime: { type: String, enum: ["months", "years"]},
+  experienceTime: { type: String, enum: ["months", "years"] },
   jobOpening: { type: Number },
   responsibilities: [],
   stipend: {
     type: String,
-    enum: ["fixed", "unpaid", "paid", "negotiable", "performanceBased"], default: 'fixed'
+    enum: ["fixed", "unpaid", "paid", "negotiable", "performanceBased"],
+    default: "fixed",
   },
   salary: { type: Number, default: null },
   salaryIn: { type: Number, default: null },
-  salaryType: { type: String, enum: ["monthly", "yearly"], default: 'monthly' },
-  salaryCtcDescription: {
-    currency: { type: String, enum: ["inr"], default: "inr" },
-    ctcFrom: { type: String, default: null },
-    ctcTo: { type: String, default: null },
-    ctcType: { type: String, default: "yearly" },
-    isProbationPeriod: { type: Boolean, default: false },
-    perks: [String]
-  },
+  salaryType: { type: String, enum: ["monthly", "yearly"], default: "monthly" },
+  currencyType: { type: String, enum: ["Inr"], default: "Inr" },
+  ctcFrom: { type: String, default: null },
+  ctcTo: { type: String, default: null },
+  ctcType: { type: String, default: "yearly" },
+  isProbationPeriod: { type: Boolean, default: false },
+  probationPeriod: [],
   perks: [],
   location: { type: String },
   status: { type: Boolean, default: false },
   coverLetter: {
     letter: { type: String, default: null },
     availability: { type: String, default: null },
-    moreQuestions: [String]
+    moreQuestions: [String],
   },
   addtionalCandidatePreference: { type: String, default: null },
   description: { type: String, default: null },
