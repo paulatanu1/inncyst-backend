17a870f4892e60cd0735f9ce1943c1bd861f4095
Manas Pramanik <manas@pitangent.com>
2024-09-04 20:07:33 +0530

    fixing

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-  const DB_URI = 'mongodb://127.0.0.1:27017/innvo';
-  // const DB_URI_ATLAS = 'mongodb+srv://immanasp:4BOeWl5tPcoF6YQ8@cluster0.9gkisrs.mongodb.net/innvo?retryWrites=true&w=majority';
+  // const DB_URI = 'mongodb://127.0.0.1:27017/innvo';
+  const DB_URI_ATLAS = 'mongodb+srv://immanasp:4BOeWl5tPcoF6YQ8@cluster0.9gkisrs.mongodb.net/innvo?retryWrites=true&w=majority';
 	const DB_USER = '';
 	const DB_PASSWORD	=	'';
 
 const connentDB = async () => {
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+    const conn = await mongoose.connect(DB_URI_ATLAS, { useNewUrlParser: true, useUnifiedTopology: true});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("Failed to connect to the server", error);
